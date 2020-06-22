18891554fee537346cc5a14f30b77b62f2d191dd
Gleider Costa <gleiderr@gmail.com>
2020-06-22 14:25:09 -0300

    docs

---
+++ docs/precache-manifest.2184c0b1b938e4341bbc128a405486a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6348f83fff476f9a347bd5dc9cff711b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a1646005df8645533a",
+    "url": "/static/css/main.594e2fef.chunk.css"
+  },
+  {
+    "revision": "f47256938a9a947a9aab",
+    "url": "/static/js/2.94b56fa4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.94b56fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a1646005df8645533a",
+    "url": "/static/js/main.ff828d17.chunk.js"
+  },
+  {
+    "revision": "cf1a3680154e735f7849",
+    "url": "/static/js/runtime-main.f7268ad0.js"
+  }
+]);
