08f1c6e5ab1ed38415c61264c8ddec529b384098
Gleider Costa <gleiderr@gmail.com>
2020-06-22 08:34:29 -0300

    Dashboard.module.css

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useHeroes } from './controller';
-import './styles/dashboard.css';
+import styles, { a, h3, h4, module, grid } from './styles/Dashboard.module.css';
 import { HeroSearch } from './HeroSearch';
+import classNames from 'classnames';
 
 export function Dashboard() {
   const [heroes] = useHeroes();
   const topHeroes = heroes.slice(1, 5);
 
+  console.log(styles);
+
   const TopHeroes = () =>
     topHeroes.map((hero) => (
-      <Link key={hero.id} className='col-1-4' to={`/detail/${hero.id}`}>
-        <div className='module hero'>
-          <h4>{hero.name}</h4>
+      <Link key={hero.id} className={classNames(styles['col-1-4'], a)} to={`/detail/${hero.id}`}>
+        <div className={classNames(module, 'hero')}>
+          <h4 className={h4}>{hero.name}</h4>
         </div>
       </Link>
     ));
 
   return (
     <>
-      <h3>Top Heroes</h3>
-      <div className='grid grid-pad'>
+      <h3 className={h3}>Top Heroes</h3>
+      <div className={classNames(grid, styles['grid-pad'])}>
         <TopHeroes />
       </div>
 
